e70cc3ed55d62e2d477e75cce4d277da21a154db
saisalokhe <saideepsalokhe4957@gmail.com>
2026-09-05 17:01:43 +0530

    Initial commit - Java automation project setup

---
+++ src/ex_01_Java_Basics/HelloWorld.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class HelloWorld {
+// main method : main tab
+    public static void main(String[] args) {
+        //print command used to print something in command
+        System.out.println(" Hello Saideep");
+    }
+}
